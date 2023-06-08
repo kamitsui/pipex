7b0a2d9dc302ec6b75b6c0f4eebb5c3cbccc52c6
Kaishiro Mitsui <kamitsui@student.42tokyo.jp>
2023-06-08 20:57:49 +0900

    Add ability to tokenize command in child_process()

--- src/multiple_pipes.c
+++ src/multiple_pipes.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 11:59:34 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/06/08 12:06:50 by kamitsui         ###   ########.fr       */
+/*   Updated: 2023/06/08 20:47:55 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@
 #include <stdio.h>// for debug
 #include <unistd.h>// for debug
 
-void	child_process(t_args args, int pipefd[2], int i)
-{
-	exit(1);
-}
-
 void	initialize_args(int argc, char **argv, char **env, t_args *args)
 {
 	args->file1 = argv[1];
@@ -31,22 +26,52 @@ void	initialize_args(int argc, char **argv, char **env, t_args *args)
 	args->env = env;
 }
 
-int	multiple_pipes(int argc, char **argv, char **env)
+//int	ft_execvp(const char *file, char *const argv[])
+//{
+//}
+
+#include <string.h>
+#define MAX_ARGS 10
+
+void	child_process(t_args *args, int pipefd[2], int i)
 {
-	t_args	args;
-	int		pipefd[2];
+	char	*command;
+	char	*arguments[MAX_ARGS];// ??
+	int		arg_count;
+	char	*token;
+
+	token = strtok(args->cmds[i], " ");
+	command = token;
+	arg_count = 0;
+	while (token != NULL && arg_count < MAX_ARGS - 1)
+	{
+		arguments[arg_count] = token;
+		arg_count++;
+		token = strtok(NULL, " ");
+	}
+	arguments[arg_count] = NULL;
+// Print the arguments
+	int	j = 0;
+	while (j < arg_count)
+	{
+		printf("Argument %d %s\n", j, arguments[j]);
+		j++;
+	}
+//	set_input();
+//	set_output();
+//	ft_execvp(file, cmd_argv);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	exit(1);
+}
+
+void	create_process(t_args *args, int pipefd[2])
+{
 	pid_t	pid;
 	int		i;
-	int		status;
 
-	initialize_args(argc, argv, env, &args);
-	if (pipe(pipefd) == -1)
-	{
-		perror("pipe");
-		exit(1);
-	}
 	i = 0;
-	while (i < args.num_cmds)
+	while (i < args->num_cmds)
 	{
 		pid = fork();
 		if (pid == -1)
@@ -56,7 +81,24 @@ int	multiple_pipes(int argc, char **argv, char **env)
 		}
 		else if (pid == 0)
 			child_process(args, pipefd, i);
+		i++;
 	}
+}
+
+int	multiple_pipes(int argc, char **argv, char **env)
+{
+	t_args	args;
+	int		pipefd[2];
+	int		i;
+	int		status;
+
+	initialize_args(argc, argv, env, &args);
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe");
+		exit(1);
+	}
+	create_process(&args, pipefd);
 	close(pipefd[0]);
 	close(pipefd[1]);
 	i = 0;
