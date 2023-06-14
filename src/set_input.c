1adfb0f8b4c9ae45d8adb2ba579787958fd5325a
Kaishiro Mitsui <kamitsui@student.42tokyo.jp>
2023-06-14 21:20:51 +0900

    Add 2 functions (for set input/output descriptor)

---
+++ src/set_input.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_input.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 14:27:16 by kamitsui          #+#    #+#             */
+/*   Updated: 2023/06/14 20:21:51 by kamitsui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+void	input_err_handle(t_pipex *pipex)
+{
+	int		err_number;
+	char	*err_message;
+
+	err_number = errno;
+	err_message = strerror(err_number);
+	printf("bash: %s: %s\n", pipex->in_file, err_message);// Must Fix
+	exit (1);
+}
+
+void	input_redirect(t_pipex *pipex)
+{
+	int		fdin;
+
+	if (access(pipex->in_file, F_OK | R_OK) == 0)
+	{
+		fdin = open(pipex->in_file, O_RDONLY);
+		if (fdin == -1)
+		{
+			perror("open");
+			exit (1);
+		}
+		if (dup2(fdin, STDIN_FILENO) == -1)
+		{
+			perror("dup2");
+			exit (1);
+		}
+		close(fdin);
+	}
+	else
+		input_err_handle(pipex);
+}
+// Must Fix
+		//ft_printf("bash: %s: %s\n", pipex->in_file, err_message);
+
+void	here_doc(t_pipex *pipex)
+{
+	if (pipex == NULL)
+		return ;
+	printf("%s\n", HERE_DOC);
+	return ;
+}
+
+void	set_input(t_pipex *pipex, int pipefd[2], int i)
+{
+	if (i != 0)
+	{
+		if (dup2(pipefd[READ_END], STDIN_FILENO) == -1)
+		{
+			perror("dup2");
+			exit (1);
+		}
+	}
+	else
+	{
+		if (pipex->mode & BIT_HERE_DOC)
+			here_doc(pipex);
+		else
+			input_redirect(pipex);
+	}
+}
---
+++ src/set_output.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_output.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 15:48:55 by kamitsui          #+#    #+#             */
+/*   Updated: 2023/06/14 20:42:04 by kamitsui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+void	output_err_handle(t_pipex *pipex)
+{
+	int		err_number;
+	char	*err_message;
+
+	err_number = errno;
+	err_message = strerror(err_number);
+	printf("bash: %s: %s\n", pipex->out_file, err_message);// Must fix
+	exit (1);
+}
+// Must Fix
+//	printf("bash: %s: %s\n", pipex->out_file, err_message);
+
+void	output_redirect(t_pipex *pipex)
+{
+	int		fdout;
+
+	if (pipex->mode & BIT_OVER_RIDE)
+		fdout = open(pipex->out_file, O_WRONLY | O_CREAT | O_TRUNC, \
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	else
+		fdout = open(pipex->out_file, O_WRONLY | O_CREAT | O_APPEND, \
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fdout == -1)
+		ft_perr_exit("open");
+	if (dup2(fdout, STDOUT_FILENO) == -1)
+		ft_perr_exit("dup2");
+	close(fdout);
+}
+
+void	set_output(t_pipex *pipex, int pipefd[2], int i)
+{
+	if (i != pipex->num_cmds - 1)
+	{
+		if (dup2(pipefd[WRITE_END], STDOUT_FILENO) == -1)
+			ft_perr_exit("dup2");
+	}
+	else
+	{
+		if (access(pipex->out_file, F_OK) == 0
+			&& access(pipex->out_file, W_OK) == -1)
+			output_err_handle(pipex);
+		else
+			output_redirect(pipex);
+	}
+}
